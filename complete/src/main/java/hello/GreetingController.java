c4fa7336c1cea5279cf72d3a526f3e5a32ecf448
Lauri Wiljami Ahonen <lwahonen@iki.fi>
2019-02-11 15:52:06 -0800

    Legacy endpoint implementation

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,12 +1,15 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.HtmlUtils;
 
@@ -16,16 +19,19 @@ import java.util.logging.Logger;
 @RestController
 @SpringBootApplication
 public class GreetingController {
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
 
-    @MessageMapping("/{name}")
-    @SendTo("/topic/{name}")
-    public Greeting greeting(@DestinationVariable String name, HelloMessage message) {
-        Logger.getLogger(getClass().getName()).log(Level.INFO, String.format("Writing %s to %s", message.getMessage(), name));
+    @MessageMapping("/{token}")
+    @SendTo("/topic/{token}")
+    public Greeting greeting(@DestinationVariable String token, HelloMessage message) {
+        Logger.getLogger(getClass().getName()).log(Level.INFO, String.format("Writing %s to %s", message.getMessage(), token));
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getMessage()) + "!");
     }
 
     @RequestMapping(path="/broker/{token}")
-    public String index(@PathVariable("token") String token) {
-        return "Greetings from "+token;
+    public String index(@PathVariable("token") String token, @RequestParam(value="message", required=false) String message) {
+        simpMessagingTemplate.convertAndSend("/topic/"+token, new Greeting(message));
+        return "Sending "+message+" to all listeners on "+token;
     }
 }
