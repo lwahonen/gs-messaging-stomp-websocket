d20752d8e3f67140452fe6a83960e90c8a98d598
Lauri Wiljami Ahonen <lwahonen@iki.fi>
2019-03-27 16:59:10 +0200

    Upgrade to a supported version of sockjs and stompjs

--- complete/src/main/resources/static/app.js
+++ complete/src/main/resources/static/app.js
@@ -14,17 +14,44 @@ function setConnected(connected) {
 
 function connect() {
     var name=$("#name").val();
-    var socket = new SockJS('/fallback');
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
+
+    stompClient = new StompJs.Client({
+        brokerURL: "notused",
+        debug: function (str) {
+            console.log(str);
+        },
+        reconnectDelay: 5000,
+        heartbeatIncoming: 4000,
+        heartbeatOutgoing: 4000
+    });
+
+    stompClient.webSocketFactory = function () {
+        // Note that the URL is different from the WebSocket URL
+        return new SockJS("/fallback");
+    };
+
+    stompClient.onConnect = function (frame) {
+        // Do something, all subscribes must be done is this callback
+        // This is needed because this will be executed after a (re)connect
         setConnected(true);
         console.log('Connected: ' + frame);
-        var destination ='/topic/'+name;
+        var destination = '/topic/' + name;
         var managed=stompClient.subscribe(destination, function (greeting) {
             showGreeting(JSON.parse(greeting.body).content);
         });
         console.log("Connection to "+managed+" with destination "+destination);
-    });
+    };
+
+    stompClient.onStompError = function (frame) {
+        // Will be invoked in case of error encountered at Broker
+        // Bad login/passcode typically will cause an error
+        // Complaint brokers will set `message` header with a brief message. Body may contain details.
+        // Compliant brokers will terminate the connection after any error
+        console.log('Broker reported error: ' + frame.headers['message']);
+        console.log('Additional details: ' + frame.body);
+    };
+
+    stompClient.activate();
 }
 
 function disconnect() {
@@ -37,7 +64,9 @@ function disconnect() {
 
 function sendName() {
     var name=$("#name").val();
-    stompClient.send("/stompbroker/"+name, {}, $("#message").val());
+    var message = $("#message").val();
+    var target = "/stompbroker/"+name;
+    stompClient.publish({destination: target, body: message});
 }
 
 function showGreeting(message) {
